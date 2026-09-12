4e9209220cec36ca71c60a164160aac114b70fa2
Kira21312312 <toranikkirill509@gmail.com>
2026-09-12 12:50:48 +0300

    Step 6: first C# program running

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет от ИСП-243");
+Console.WriteLine("Автор: Торяник Кирилл");
+Console.WriteLine("Год: 2026");
