5d990702387dd69bd2d75c4b41f549c342b902eb
Kira21312312 <toranikkirill509@gmail.com>
2026-09-12 13:13:30 +0300

    Step 7: variables and data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,3 +2,24 @@
 Console.WriteLine("Привет от ИСП-243");
 Console.WriteLine("Автор: Торяник Кирилл");
 Console.WriteLine("Год: 2026");
+
+int age = 20;
+double gpa = 4.5;
+bool isStudent = true;
+string name = "Кирилл";
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(gpa);
+Console.WriteLine(isStudent);
+
+var city = "Москва";
+var year = 2026;
+var pi = 3.123123412;
+var isActive = true;
+
+Console.WriteLine($"Меня зовут {city}, мне {year} лет, я учусь в группе {pi}");
+
+Console.Write("Введите ваше имя: ");
+string name2 = Console.ReadLine();
+Console.WriteLine($"Привет {name2}");
